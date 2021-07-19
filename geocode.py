c7f4a5ffc4270561d69f3699d3220dc9eab5f7d5
remdelaportemathurin <delaporte.mathurin@laposte.net>
2021-07-19 20:15:54 +0200

    added radii

--- geocode.py
+++ geocode.py
@@ -3,7 +3,7 @@ import csv
 import json
 
 # Now we create stores.geojson, which is the GeoJSON equivalent of stores.csv
-with open('tokamaks2.json') as f:
+with open('tokamaks.json') as f:
     stores = json.load(f)
 
 geojson = {
@@ -27,6 +27,8 @@ for store in stores:
     if "website" in store:
         content = "<a href='" + store["website"] + "'> Website </a>"
         feature_dict['properties']['popupContent'] = content
+    if "R" in store:
+        feature_dict['properties']['R'] = store["R"]
     geojson["features"].append(feature_dict)
 
 with open('tokamaks.geojson', 'w') as f:
