dd37b78b9e3c3cb8c9a18737a1779559e02a8a1d
remdelaportemathurin <delaporte.mathurin@laposte.net>
2021-07-19 19:33:08 +0200

    more appealing json format

--- geocode.py
+++ geocode.py
@@ -3,8 +3,8 @@ import csv
 import json
 
 # Now we create stores.geojson, which is the GeoJSON equivalent of stores.csv
-with open('tokamaks.csv') as f:
-    stores = list(csv.DictReader(f))
+with open('tokamaks2.json') as f:
+    stores = json.load(f)
 
 geojson = {
     'type': 'FeatureCollection',
@@ -21,16 +21,13 @@ for store in stores:
         },
         'geometry': {
             'type': 'Point',
-            'coordinates': [
-                float(store['longitude']),
-                float(store['latitude'])
-            ]
+            'coordinates': store['coordinates'],
         }
     }
-    if store["website"] != "":
+    if "website" in store:
         content = "<a href='" + store["website"] + "'> Website </a>"
         feature_dict['properties']['popupContent'] = content
     geojson["features"].append(feature_dict)
 
 with open('tokamaks.geojson', 'w') as f:
-    json.dump(geojson, f)
+    json.dump(geojson, f, indent=4)
