ae41ba26a834a6acb63a139098f523d372da4cf1
remdelaportemathurin <rdelaportemathurin@gmail.com>
2021-12-14 15:44:43 +0100

    master to main

--- stats.js
+++ stats.js
@@ -47,7 +47,7 @@ var options_bar = {
 
 async function drawBarChart() {
     var countries = [];
-    const data_countries = await fetch('https://raw.githubusercontent.com/RemDelaporteMathurin/fusion-world/master/machines_by_country.json').then((r)=>r.json());
+    const data_countries = await fetch('https://raw.githubusercontent.com/RemDelaporteMathurin/fusion-world/main/machines_by_country.json').then((r)=>r.json());
     // waits until the request completes...
     var series = [
         {
